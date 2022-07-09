980b6f0c6520854f732565211f26c19bfbcec3c3
frank <yantao0527@gmail.com>
2022-07-09 13:35:39 +0000

    fixed acl warning

--- iac/backend_pipeline/s3_bucket.tf
+++ iac/backend_pipeline/s3_bucket.tf
@@ -1,19 +1,27 @@
 # S3 bucket used by CodePipeline for artifacts
 resource "aws_s3_bucket" "pipeline_bucket" {
   bucket = "${var.pipeline_bucket_name}-${var.environment}"
-  acl    = var.bucket_acl
 
   tags = {
     Name = "${var.pipeline_bucket_name}-${var.environment}"
   }
 }
 
+resource "aws_s3_bucket_acl" "pipeline_bucket_acl" {
+  bucket = aws_s3_bucket.pipeline_bucket.id
+  acl    = "private"
+}
+
 # S3 bucket used by CodeBuild for cache
 resource "aws_s3_bucket" "codebuild_bucket" {
   bucket = "${var.codebuild_bucket_name}-${var.environment}"
-  acl    = var.bucket_acl
 
   tags = {
     Name = "${var.codebuild_bucket_name}-${var.environment}"
   }
+}
+
+resource "aws_s3_bucket_acl" "codebuild_bucket_acl" {
+  bucket = aws_s3_bucket.codebuild_bucket.id
+  acl    = "private"
 }
